1e977b6dc75bd2685d8df4e745a45f42c00cde0a
alicja-brzezowska <brzezowskaalicja0@gmail.com>
2026-02-15 12:46:08 +0000

    factor out main painter function

---
+++ tszpaint/paint/main.py
@@ -0,0 +1,51 @@
+from loguru import logger
+
+from tszpaint.config import HALO_CATALOGS_PATH, HEALCOUNTS_PATH, INTERPOLATORS_PATH
+from tszpaint.paint.config import PainterConfig
+from tszpaint.paint.paint import paint_abacus
+
+# HEALPix
+NSIDE = 8192
+Z = 0.5  # FOR MOCK DATA
+N = 2  # Multiple of theta_200 to search
+N_BINS = 20  # NOTE: THINK how many bins!
+USE_WEIGHTS = True
+
+
+def main():
+    halo_dir = HALO_CATALOGS_PATH / "z0.542" / "lightcone_halo_info_000.asdf"
+    healcounts_file1 = (
+        HEALCOUNTS_PATH / "LightCone0_halo_heal-counts_Step0671-0676.asdf"
+    )
+    healcounts_file2 = (
+        HEALCOUNTS_PATH / "LightCone0_halo_heal-counts_Step0677-0682.asdf"
+    )
+    healcounts_file3 = (
+        HEALCOUNTS_PATH / "LightCone0_halo_heal-counts_Step0665-0670.asdf"
+    )
+    JAX_PATH = INTERPOLATORS_PATH / "y_values_jax_2.pkl"
+    output_file = "y_map_abacus.fits"
+
+    logger.info("Painting Abacus tSZ map...")
+    logger.info(f"Halo directory: {halo_dir}")
+    logger.info(f"Healcounts file 1: {healcounts_file1}")
+    logger.info(f"Healcounts file 2: {healcounts_file2}")
+    logger.info(f"Healcounts file 3: {healcounts_file3}")
+    logger.info(f"Output file: {output_file}")
+    config = PainterConfig(NSIDE, N, N_BINS)
+
+    paint_abacus(
+        config,
+        halo_dir=halo_dir,
+        healcounts_file_1=healcounts_file1,
+        healcounts_file_2=healcounts_file2,
+        healcounts_file_3=healcounts_file3,
+        output_file=output_file,
+        nside=NSIDE,
+        interpolator_path=JAX_PATH,
+        use_weights=USE_WEIGHTS,
+    )
+
+
+if __name__ == "__main__":
+    main()
